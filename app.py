55e6c9d9b5d8f36c4046422ff180b05f9ce3c2fc
Elena Fedchenko <fedchenko.yelena@gmail.com>
2020-06-13 21:12:16 +1200

    Added functions 4-7

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
 from functions import *
 
 def menu():
-    options = int(input("""Choose the excercise:
-                    1) Simple Array Sum
-                    2) Compare triplets
-                    3) Cats and a mouse\n"""))
+    while True:
+        option = input("""\nChoose the excercise:
+                        1) Simple Array Sum
+                        2) Compare triplets
+                        3) Cats and a mouse
+                        4) Electronics Shop
+                        5) Day of the Programmer
+                        6) Time Conversion
+                        7) Grading students
 
-    if options == 1:
-        print(simpleArraySum())
+                        x to quit\n""")
 
-    if options == 2:
-        print(compareTriplets())
+        if option == "1":
+            print(simpleArraySum())
 
-    if options == 3:
-        print(catAndMouse())
+        if option == "2":
+            print(compareTriplets())
+
+        if option == "3":
+            print(catAndMouse())
+
+        if option == "4":
+            print(getMoneySpent())
+
+        if option == "5":
+            print(dayOfProgrammer())
+
+        if option == "6":
+            print(timeConversion())
+
+        if option == "7":
+            print(gradingStudents())
+
+        if option == "x":
+            break
 
 
 if __name__ == "__main__":
--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 # 1) Given an array of integers, find the sum of its elements.
 # https://www.hackerrank.com/challenges/simple-array-sum/problem
 
@@ -44,3 +46,75 @@ def catAndMouse():
         return "Cat B"
     else:
         return "Mouse C"
+
+# 4) If two items are in the budget, return the amount of money spent. If not, return -1
+# https://www.hackerrank.com/challenges/electronics-shop/problem
+
+def getMoneySpent():
+    bnm = input("Enter budget, the number of keyboard models and the number of USB drive models (e.g.: 10 2 3):\n").split()
+
+    b = int(bnm[0])
+    n = int(bnm[1])
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input("Enter the prices of each keyboard model (e.g.: 3 1):\n").rstrip().split()))
+    drives = list(map(int, input("Enter the prices of the USB drives.(e.g.: 5 2 8):\n").rstrip().split()))
+
+    return max(m+n if m+n<=b else -1 for m, n in product(keyboards, drives))
+
+# 5) Marie invented a Time Machine and wants to test it by time-traveling to visit Russia on the Day of the Programmer
+#(the 256th day of the year) during a year in the inclusive range from 1700 to 2700.
+# From 1700 to 1917, Russia's official calendar was the Julian calendar; since 1919 they used the Gregorian calendar system.
+# https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+
+def dayOfProgrammer():
+    year = int(input("""Choose a year to find the Day of the Programmer (the 256th day of the year) in Russia.
+    Bear in mind that From 1700 to 1917, Russia's official calendar was the Julian calendar:\n""").strip())
+
+    leap = 244
+    non_leap = 243
+    x = 256
+
+    if year == 1918:
+        return "26.09.1918"
+    else:
+        if year % 4 == 0 and (year < 1918 or year % 100 != 0 or year % 400 == 0):
+            day = x - leap
+            return f"{day}.09.{year}"
+        else:
+            day = x - non_leap
+            return f"{day}.09.{year}"
+
+# 6) Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+# https://www.hackerrank.com/challenges/time-conversion/problem
+
+def timeConversion():
+    s = input("Choose a time in 12-hour AM/PM format to convert it to military (24-hour) time (e.g.: 07:05:45PM):\n")
+    if s[8:] == "PM":
+        if s[:2] == "12":
+            y = int(s[:2])
+        else:
+            y = int(s[:2]) + 12
+
+    elif s[8:] == "AM":
+        if s[:2] == "12":
+            y = int(s[:2]) - 12
+            y = f"{y:02d}"
+        else:
+            y = s[:2]
+    final = str(y) + s[2:8]
+    return final
+
+# 7) Round the grades according to the rules on:
+# https://www.hackerrank.com/challenges/grading/problem
+
+def gradingStudents():
+    grades_count = int(input("Enter the number of students: ").strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input("Grade: ").strip())
+        grades.append(grades_item)
+
+    return [n if n < 38 or n % 5 < 3 else (n + 5 - (n % 5)) for n in grades]
